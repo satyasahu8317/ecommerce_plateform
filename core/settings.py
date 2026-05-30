4a8c72be1b5b7f6d5c669ad22d900290f0cdb57a
satyasahu8317 <sahusatya8317@gmail.com>
2026-05-31 01:27:30 +0530

    Implement PostgreSQL full-text search and category filtering

--- core/settings.py
+++ core/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.postgres',
     
     # Cloudinary Storage (Must be ABOVE staticfiles)
     'cloudinary_storage',
--- products/api/views/product_views.py
+++ products/api/views/product_views.py
@@ -1,4 +1,6 @@
 # pyrefly: ignore [missing-import]
+from django.contrib.postgres.search import SearchVector
+# pyrefly: ignore [missing-import]
 from rest_framework import generics
 from products.models import Product
 from ..serializers.product_serializers import ProductSerializer
@@ -8,10 +10,15 @@ class ProductListView(generics.ListAPIView):
 
     def get_queryset(self):
         queryset = Product.objects.filter(in_stock=True)
-        category_slug = self.request.query_params.get('category_slug')
+        query = self.request.query_params.get('search')
+        if query:
+            queryset = queryset.annotate(
+                search=SearchVector('name', 'description'),
+            ).filter(search=query)
 
+        category_slug = self.request.query_params.get('category_slug')
         if category_slug:
-            queryset = queryset.filter(category__slug=category_slug)
+            queryset = queryset.filter(category__slug=category_slug)    
         
         return queryset
 
